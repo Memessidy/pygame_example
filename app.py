efcf29c5de097288aacd7d6c1848d63935b6ee37
Memessidy <>
2023-06-30 23:23:05 +0300

    app file added

---
+++ app.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Space invaders")
+    bg_color = (0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+run()
+
