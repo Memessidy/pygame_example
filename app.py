0f223f47e1ee3bd8fa944fc29b4a86e15f12c2b1
Memessidy <>
2023-07-01 01:35:28 +0300

    gun can move now

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import pygame
-import sys
 from gun import Gun
+import controls
 
 
 def run():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((700, 800))
     pygame.display.set_caption("Space invaders")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        controls.events(gun)
+        gun.update_gun()
         screen.fill(bg_color)
         gun.output()
         pygame.display.flip()
--- gun.py
+++ gun.py
@@ -11,8 +11,18 @@ class Gun:
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.mright = False
+        self.mleft = False
 
     def output(self):
         """рисование пушки"""
         self.screen.blit(self.image, self.rect)
+
+    def update_gun(self):
+        """обновление позиции пушки"""
+        if self.mright and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+
+        if self.mleft and self.rect.left > 0:
+            self.rect.centerx -= 1
 
